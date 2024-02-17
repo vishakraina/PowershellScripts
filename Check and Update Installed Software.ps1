6d91f7b60d0029c535ddcc3826c713c69d7f57f3
vishakraina <78915709+vishakraina@users.noreply.github.com>
2024-02-17 20:08:29 +0530

    Add files via upload
    Get-WmiObject -Class Win32_Product: This cmdlet retrieves instances of the Win32_Product class, which contains information about installed software products on the system.
    
    ForEach-Object { ... }: This is a loop construct that iterates over each object returned by the Get-WmiObject cmdlet.
    
    $name = $_.Name: Within the loop, $_ represents the current object being processed. In this case, it's an instance of the Win32_Product class. $_.Name retrieves the name of the software product and assigns it to the $name variable.
    
    $version = $_.Version: Similarly, $_.Version retrieves the version of the software product and assigns it to the $version variable.
    
    Write-Host "$name - Version $version": This line prints out the name and version of each software product to the console. It uses string interpolation to include the values of the $name and $version variables within the string.

---
+++ Check and Update Installed Software.ps1
@@ -0,0 +1,8 @@
+﻿# Check and Update Installed Software
+
+Get-WmiObject -Class Win32_Product | ForEach-Object {
+    $name = $_.Name
+    $version = $_.Version
+    # Check for updates or perform other actions
+    Write-Host "$name - Version $version"
+}
