d9ec83c1a72397df470a418a379652734ede837e
vishakraina <78915709+vishakraina@users.noreply.github.com>
2024-02-17 19:12:31 +0530

    Add files via upload
    Check disk space and give a print output of free space on the disk

---
+++ Check Disk Space and Alert if Below Threshold.ps1
@@ -0,0 +1,11 @@
+﻿#Check Disk Space and Alert if Below Threshold:
+
+$threshold = 80 #Set threshold percentage
+$diskspace = Get-WmiObject -Class Win32_LogicalDisk | Where-Object{$_.Drivetype -eq 3}
+foreach($disk in $diskspace){
+    $freespacepercent=[Math]::Round(($disk.freespace /$disk.size) * 100,2)
+    if ($freeSpacePercent -lt $threshold) {
+        Write-Host "Disk $($disk.DeviceID) has low free space: $($freeSpacePercent)%"
+        # Send alert or take action here
+    }
+}
