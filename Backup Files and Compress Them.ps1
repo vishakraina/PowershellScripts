6a1a0638bafbfe8aa6f9cc5ae9dc4922aa88c742
vishakraina <78915709+vishakraina@users.noreply.github.com>
2024-02-17 19:21:06 +0530

    Add files via upload
    Backup Files and Compress Them. Here source and destination folder should be created to get the compressed output.

---
+++ Backup Files and Compress Them.ps1
@@ -0,0 +1,5 @@
+﻿#Backup Files and Compress Them
+$sourceDir = "C:\Source"
+$backupDir = "C:\Backup"
+$backupFileName = "Backup_$(Get-Date -Format 'yyyyMMdd').zip"
+Compress-Archive -Path $sourceDir -DestinationPath "$backupDir\$backupFileName"
