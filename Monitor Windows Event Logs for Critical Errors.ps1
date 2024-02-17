c00cd31f37e5a9bea25a34868dec12dfd383e06c
vishakraina <78915709+vishakraina@users.noreply.github.com>
2024-02-18 01:52:42 +0530

    Add files via upload

---
+++ Monitor Windows Event Logs for Critical Errors.ps1
@@ -0,0 +1,8 @@
+﻿# Monitor Windows Event Logs for Critical Errors:
+
+$criticalEvents = Get-EventLog -LogName System -EntryType Error | Where-Object {$_.EventID -eq 6008 -or $_.EventID -eq 41}
+if ($criticalEvents) {
+    Write-Host "Critical errors detected in the system event log:"
+    $criticalEvents | Format-Table -AutoSize
+    # Send alert or take action here
+}
